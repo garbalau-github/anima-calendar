647a35d6d7d6349acaf1099b713cd3b6cb555ba9
Nick Garbalau <garbalaunick@gmail.com>
2023-05-25 11:18:04 +0300

    Updated url for axios

--- calendar-client/src/axios/api.js
+++ calendar-client/src/axios/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const API_URL = axios.create({
-    baseURL: 'http://localhost:5000/api/events',
+    baseURL: 'https://anima-calendar-server.onrender.com/api/events',
 });
